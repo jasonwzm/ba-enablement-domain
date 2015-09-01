00ae9bca2374e5fefe8ca92d11a130d49c982028
Jason Wang <jasonwan@redhat.com>
2015-09-01 17:23:14 -0400

    added rules

--- ba-enablement-language/src/main/java/com/redhat/RuleResponse.java
+++ ba-enablement-language/src/main/java/com/redhat/RuleResponse.java
@@ -7,8 +7,26 @@ public class RuleResponse {
 	@KieQuery(binding = "$business", queryName = "Get Business")
 	private Business business;
 	
+	@KieQuery(binding = "$responseCode", queryName = "getresponseCode")
 	private String responseCode; // e.g. success, failure, filtered, validation error...
 	
+	public String getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseCode(String responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	public Collection<Reason> getReasons() {
+		return reasons;
+	}
+
+	public void setReasons(Collection<Reason> reasons) {
+		this.reasons = reasons;
+	}
+
+	@KieQuery(binding = "$reason", queryName = "getReasons")
 	private Collection<Reason> reasons; // messages about why the response Code came bacl
 
 	public Business getBusiness() {
